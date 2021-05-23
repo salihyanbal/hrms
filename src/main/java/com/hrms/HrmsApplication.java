0e66a2952eb36684f8ed844d5c22e463ced59c59
Luciko <salixynbl@gmail.com>
2021-05-23 14:59:05 +0300

    Initial commit

--- src/main/java/com/hrms/HrmsApplication.java
+++ src/main/java/com/hrms/HrmsApplication.java
@@ -20,7 +20,7 @@ public class HrmsApplication {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.northwind"))
+                .apis(RequestHandlerSelectors.basePackage("com.hrms"))
                 .build();
     }
 
